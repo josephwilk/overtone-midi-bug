9d04a519890049dbc47ad9d062b08f2e53300e7e
Joseph Wilk <joe@josephwilk.net>
2013-12-24 12:02:46 +0100

    simplify

--- src/midi_bug/core.clj
+++ src/midi_bug/core.clj
@@ -12,56 +12,27 @@
   [bank-k]
   (case bank-k
     :master btn/record
-    :lp64   btn/play
-    ;;      btn/stop    ; stop
-    :riffs  btn/fast-forward ; fast-forward
-    :synths btn/rewind)) ; rewind
+    :synths btn/rewind))
 
 (def cfg
-  {:synths {:s0 mixer-init-state :s1 mixer-init-state :s2 mixer-init-state :m0 mixer-init-state :m1 mixer-init-state :r0 mixer-init-state :r7 basic-mixer-init-state}
-   :riffs  {:s0 mixer-init-state :s1 mixer-init-state :m0 mixer-init-state :m1 mixer-init-state :r7 basic-mixer-init-state}
+  {:synths {:s0 mixer-init-state}
    :master {:s7 mixer-init-state :m7 mixer-init-state :r7 mixer-init-state}})
 
 (def banks
-  {:master btn/record
-   :lp64   btn/play
-   :riffs  btn/fast-forward
-   :synths btn/rewind})
+  {:master btn/record :synths btn/rewind})
 
 (nk2/start! banks cfg)
 
 (defonce default-mixer-g (group :tail (foundation-safe-post-default-group)))
 
 (defonce synth-bus (audio-bus 2))
-(defonce riffs-bus (audio-bus 2))
-
 (defonce mixer-s0 (mixers/add-nk-mixer (nk-bank :synths) :s0 default-mixer-g synth-bus))
-(defonce mixer-s1 (mixers/add-nk-mixer (nk-bank :synths) :s1 default-mixer-g synth-bus))
-(defonce mixer-m0 (mixers/add-nk-mixer (nk-bank :synths) :m0 default-mixer-g synth-bus))
-(defonce mixer-m1 (mixers/add-nk-mixer (nk-bank :synths) :m1 default-mixer-g synth-bus))
-(defonce mixer-s2 (mixers/add-nk-mixer (nk-bank :synths) :s2 default-mixer-g synth-bus))
-(defonce mixer-r0 (mixers/add-nk-mixer (nk-bank :synths) :r0 default-mixer-g synth-bus))
-
 (defonce basic-synth-mix (mixers/basic-mixer [:after default-mixer-g] :in-bus synth-bus))
-(defonce basic-riffs-mix (mixers/basic-mixer [:after default-mixer-g] :in-bus riffs-bus))
-
-(defonce mixer-riff-s0 (mixers/add-nk-mixer (nk-bank :riffs) :s0 default-mixer-g riffs-bus))
-(defonce mixer-riff-s1 (mixers/add-nk-mixer (nk-bank :riffs) :s1 default-mixer-g riffs-bus))
-(defonce mixer-riff-m0 (mixers/add-nk-mixer (nk-bank :riffs) :m0 default-mixer-g riffs-bus))
-(defonce mixer-riff-m1 (mixers/add-nk-mixer (nk-bank :riffs) :m1 default-mixer-g riffs-bus))
-(defonce mixer-riff-s2 (mixers/add-nk-mixer (nk-bank :riffs) :s2 default-mixer-g riffs-bus))
-(defonce mixer-riff-r0 (mixers/add-nk-mixer (nk-bank :riffs) :r0 default-mixer-g riffs-bus))
 
 (on-latest-event [:v-nanoKON2 (nk-bank :synths) :r7 :control-change :slider7]
                  (fn [{:keys [val]}]
                    (ctl basic-synth-mix :amp val))
                  ::synths-master-amp)
-
-(on-latest-event [:v-nanoKON2 (nk-bank :riffs) :r7 :control-change :slider7]
-                 (fn [{:keys [val]}]
-                   (ctl basic-riffs-mix :amp val))
-                 ::riffs-master-amp)
-
 
 (defsynth high-space-organ [out-bus 0 amp 1 size 200 r 8 noise 10 trig 0 t0 8 t1 16 t2 24 d0 1 d1 1/2 d2 1/4 d3 1/8]
   (let [notes (map #(midicps (duty:kr % (mod trig 16) (dseq [10 20 30] INF))) [d0 d1 d2 d3])
