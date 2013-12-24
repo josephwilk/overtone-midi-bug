e2fb117b36deaec450d117895f336de4964febac
Joseph Wilk <joe@josephwilk.net>
2013-12-24 12:08:52 +0100

    more simplifying

--- src/midi_bug/core.clj
+++ src/midi_bug/core.clj
@@ -18,8 +18,7 @@
   {:synths {:s0 mixer-init-state}
    :master {:s7 mixer-init-state :m7 mixer-init-state :r7 mixer-init-state}})
 
-(def banks
-  {:master btn/record :synths btn/rewind})
+(def banks {:master btn/record :synths btn/rewind})
 
 (nk2/start! banks cfg)
 
@@ -28,11 +27,6 @@
 (defonce synth-bus (audio-bus 2))
 (defonce mixer-s0 (mixers/add-nk-mixer (nk-bank :synths) :s0 default-mixer-g synth-bus))
 (defonce basic-synth-mix (mixers/basic-mixer [:after default-mixer-g] :in-bus synth-bus))
-
-(on-latest-event [:v-nanoKON2 (nk-bank :synths) :r7 :control-change :slider7]
-                 (fn [{:keys [val]}]
-                   (ctl basic-synth-mix :amp val))
-                 ::synths-master-amp)
 
 (defsynth high-space-organ [out-bus 0 amp 1 size 200 r 8 noise 10 trig 0 t0 8 t1 16 t2 24 d0 1 d1 1/2 d2 1/4 d3 1/8]
   (let [notes (map #(midicps (duty:kr % (mod trig 16) (dseq [10 20 30] INF))) [d0 d1 d2 d3])
